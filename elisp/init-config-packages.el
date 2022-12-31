f3aae82b814f0598f996326ff11f8be083b86b94
19年梦醒 <3949379+getong@users.noreply.github.com>
2022-12-31 22:33:56 +0800

    disable whitespace package

--- elisp/init-config-packages.el
+++ elisp/init-config-packages.el
@@ -1071,53 +1071,53 @@ The cursor becomes a blinking bar, per `prot/cursor-type-mode'."
 
 ;; 显示空白字符，如 \t \v \v 空格等等。
 ;; 可以配置在 prog-mode，markdown-mode 和 conf-mode 下，显示行尾的空白字符。
-(use-package whitespace
-  :ensure nil
-  :hook
-  (after-init . global-whitespace-mode)
-
-  ((prog-mode markdown-mode conf-mode) . whitespace-mode)
-  :config
-  ;; makefile等以tab为标识的文件中也会将tab转换为空格, 排除特定的mode呢
-  (setq whitespace-global-modes '(not makefile-mode))
-  ;; Don't use different background for tabs.
-  (face-spec-set 'whitespace-tab
-                 '((t :background unspecified)))
-  ;; Only use background and underline for long lines, so we can still have
-  ;; syntax highlight.
-
-  ;; For some reason use face-defface-spec as spec-type doesn't work.  My guess
-  ;; is it's due to the variables with the same name as the faces in
-  ;; whitespace.el.  Anyway, we have to manually set some attribute to
-  ;; unspecified here.
-  (face-spec-set 'whitespace-line
-                 '((((background light))
-                    :background "#d8d8d8" :foreground unspecified
-                    :underline t :weight unspecified)
-                   (t
-                    :background "#404040" :foreground unspecified
-                    :underline t :weight unspecified)))
-
-  ;; Use softer visual cue for space before tabs.
-  (face-spec-set 'whitespace-space-before-tab
-                 '((((background light))
-                    :background "#d8d8d8" :foreground "#de4da1")
-                   (t
-                    :inherit warning
-                    :background "#404040" :foreground "#ee6aa7")))
-
-  (setq
-   whitespace-line-column nil
-   whitespace-style
-   '(face             ; visualize things below:
-     empty            ; empty lines at beginning/end of buffer
-     lines-tail       ; lines go beyond `fill-column'
-     space-before-tab ; spaces before tab
-     trailing         ; trailing blanks
-     tabs             ; tabs (show by face)
-     tab-mark         ; tabs (show by symbol)
-     ))
-  )
+;;(use-package whitespace
+;;  :ensure nil
+;;  :hook
+;;  (after-init . global-whitespace-mode)
+;;
+;;  ((prog-mode markdown-mode conf-mode) . whitespace-mode)
+;;  :config
+;;  ;; makefile等以tab为标识的文件中也会将tab转换为空格, 排除特定的mode呢
+;;  (setq whitespace-global-modes '(not makefile-mode))
+;;  ;; Don't use different background for tabs.
+;;  (face-spec-set 'whitespace-tab
+;;                 '((t :background unspecified)))
+;;  ;; Only use background and underline for long lines, so we can still have
+;;  ;; syntax highlight.
+;;
+;;  ;; For some reason use face-defface-spec as spec-type doesn't work.  My guess
+;;  ;; is it's due to the variables with the same name as the faces in
+;;  ;; whitespace.el.  Anyway, we have to manually set some attribute to
+;;  ;; unspecified here.
+;;  (face-spec-set 'whitespace-line
+;;                 '((((background light))
+;;                    :background "#d8d8d8" :foreground unspecified
+;;                    :underline t :weight unspecified)
+;;                   (t
+;;                    :background "#404040" :foreground unspecified
+;;                    :underline t :weight unspecified)))
+;;
+;;  ;; Use softer visual cue for space before tabs.
+;;  (face-spec-set 'whitespace-space-before-tab
+;;                 '((((background light))
+;;                    :background "#d8d8d8" :foreground "#de4da1")
+;;                   (t
+;;                    :inherit warning
+;;                    :background "#404040" :foreground "#ee6aa7")))
+;;
+;;  (setq
+;;   whitespace-line-column nil
+;;   whitespace-style
+;;   '(face             ; visualize things below:
+;;     empty            ; empty lines at beginning/end of buffer
+;;     lines-tail       ; lines go beyond `fill-column'
+;;     space-before-tab ; spaces before tab
+;;     trailing         ; trailing blanks
+;;     tabs             ; tabs (show by face)
+;;     tab-mark         ; tabs (show by symbol)
+;;     ))
+;;  )
 
 ;; 当打开一个具有长行的文件时，它会自动检测并将一些可能导致严重性能的 mode 关闭， 如 syntax highlight。
 (use-package so-long

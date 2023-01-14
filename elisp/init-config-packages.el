d498db598347b5b351bbdc4fd5da57b44cc82425
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-01-15 04:01:35 +0800

    update emacs config

--- elisp/init-config-packages.el
+++ elisp/init-config-packages.el
@@ -6,15 +6,15 @@
   :pin melpa)
 
 ;; copy from https://www.reddit.com/r/emacs/comments/iu0euj/getting_modern_multiple_cursors_in_emacs/
-(use-package multiple-cursors
-  :ensure   t
-  :bind (("H-SPC" . set-rectangular-region-anchor)
-         ("C-M-SPC" . set-rectangular-region-anchor)
-         ("C->" . mc/mark-next-like-this)
-         ("C-<" . mc/mark-previous-like-this)
-         ("C-c C->" . mc/mark-all-like-this)
-         ("C-c C-SPC" . mc/edit-lines)
-         ))
+;;(use-package multiple-cursors
+;;  :ensure   t
+;;  :bind (("H-SPC" . set-rectangular-region-anchor)
+;;         ("C-M-SPC" . set-rectangular-region-anchor)
+;;         ("C->" . mc/mark-next-like-this)
+;;         ("C-<" . mc/mark-previous-like-this)
+;;         ("C-c C->" . mc/mark-all-like-this)
+;;         ("C-c C-SPC" . mc/edit-lines)
+;;         ))
 
 ;; copy from [Error when running magit-status: run-hooks: Wrong number of arguments](https://github.com/magit/magit/issues/3837)
 (use-package transient
@@ -98,29 +98,84 @@ The cursor becomes a blinking bar, per `prot/cursor-type-mode'."
 ;;(advice-add 'undo-tree-visualizer-mouse-set :after #'undo-tree-visualizer-update-linum)
 ;;(advice-add 'undo-tree-visualizer-set :after #'undo-tree-visualizer-update-linum)
 
-(use-package vundo
-  :bind ("C-x u" . vundo)
-  :commands (vundo)
-  :hook ((vundo-mode . my/vundo-setup))
-  :init
-  (progn
-    (setq vundo-window-max-height 5))
-  :config
-  (progn
-    (setq vundo-glyph-alist vundo-unicode-symbols)
-    ;; Take less on-screen space.
-    (setq vundo-compact-display t)
-    ;; Better contrasting highlight.
-    (custom-set-faces
-     '(vundo-node ((t (:foreground "#808080"))))
-     '(vundo-stem ((t (:foreground "#808080"))))
-     '(vundo-highlight ((t (:foreground "#FFFF00")))))
+;;(use-package vundo
+;;  :bind ("C-x u" . vundo)
+;;  :commands (vundo)
+;;  :hook ((vundo-mode . my/vundo-setup))
+;;  :init
+;;  (progn
+;;    (setq vundo-window-max-height 5))
+;;  :config
+;;  (progn
+;;    (setq vundo-glyph-alist vundo-unicode-symbols)
+;;    ;; Take less on-screen space.
+;;    (setq vundo-compact-display t)
+;;    ;; Better contrasting highlight.
+;;    (custom-set-faces
+;;     '(vundo-node ((t (:foreground "#808080"))))
+;;     '(vundo-stem ((t (:foreground "#808080"))))
+;;     '(vundo-highlight ((t (:foreground "#FFFF00")))))
+;;
+;;    (defun my/vundo-setup ()
+;;      "Remove mode-line and header-line."
+;;      (setq mode-line-format nil)
+;;      (setq header-line-format nil))
+;;    ))
 
-    (defun my/vundo-setup ()
-      "Remove mode-line and header-line."
-      (setq mode-line-format nil)
-      (setq header-line-format nil))
-    ))
+(use-package hydra
+  :ensure t)
+
+(use-package use-package-hydra
+  :ensure t
+  :after hydra)
+
+(use-package undo-tree
+  :ensure t
+  :init (global-undo-tree-mode)
+  :after hydra
+  :bind ("C-x C-h u" . hydra-undo-tree/body)
+  :hydra (hydra-undo-tree (:hint nil)
+                          "
+  _p_: undo  _n_: redo _s_: save _l_: load   "
+                          ("p"   undo-tree-undo)
+                          ("n"   undo-tree-redo)
+                          ("s"   undo-tree-save-history)
+                          ("l"   undo-tree-load-history)
+                          ("u"   undo-tree-visualize "visualize" :color blue)
+                          ("q"   nil "quit" :color blue)))
+
+(use-package multiple-cursors
+  :ensure t
+  :after hydra
+  :bind
+  (("C-x C-h m" . hydra-multiple-cursors/body)
+   ("C-S-<mouse-1>" . mc/toggle-cursor-on-click))
+  :hydra (hydra-multiple-cursors
+		  (:hint nil)
+		  "
+Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cursor%s(if (> (mc/num-cursors) 1) \"s\" \"\")
+------------------------------------------------------------------
+ [_p_]   Prev     [_n_]   Next     [_l_] Edit lines  [_0_] Insert numbers
+ [_P_]   Skip     [_N_]   Skip     [_a_] Mark all    [_A_] Insert letters
+ [_M-p_] Unmark   [_M-n_] Unmark   [_s_] Search      [_q_] Quit
+ [_|_] Align with input CHAR       [Click] Cursor at point"
+		  ("l" mc/edit-lines :exit t)
+		  ("a" mc/mark-all-like-this :exit t)
+		  ("n" mc/mark-next-like-this)
+		  ("N" mc/skip-to-next-like-this)
+		  ("M-n" mc/unmark-next-like-this)
+		  ("p" mc/mark-previous-like-this)
+		  ("P" mc/skip-to-previous-like-this)
+		  ("M-p" mc/unmark-previous-like-this)
+		  ("|" mc/vertical-align)
+		  ("s" mc/mark-all-in-region-regexp :exit t)
+		  ("0" mc/insert-numbers :exit t)
+		  ("A" mc/insert-letters :exit t)
+		  ("<mouse-1>" mc/add-cursor-on-click)
+		  ;; Help with click recognition in this hydra
+		  ("<down-mouse-1>" ignore)
+		  ("<drag-mouse-1>" ignore)
+		  ("q" nil)))
 
 ;; copy from https://emacs-china.org/t/vterm-zsh/20497
 ;;; Terminal
@@ -1950,5 +2005,33 @@ The cursor becomes a blinking bar, per `prot/cursor-type-mode'."
   :after flycheck
   :config
   (flycheck-package-setup))
+
+(use-package dashboard
+  :ensure t
+  :config
+  (setq dashboard-banner-logo-title "Welcome to Emacs!") ;; 个性签名，随读者喜好设置
+  (setq dashboard-projects-backend 'projectile) ;; 读者可以暂时注释掉这一行，等安装了 projectile 后再使用
+  (setq dashboard-startup-banner 'official) ;; 也可以自定义图片
+  (setq dashboard-items '((recents  . 5)   ;; 显示多少个最近文件
+			              (bookmarks . 5)  ;; 显示多少个最近书签
+			              (projects . 10))) ;; 显示多少个最近项目
+  (dashboard-setup-startup-hook))
+
+(use-package highlight-symbol
+  :ensure t
+  :init (highlight-symbol-mode)
+  :bind ("<f3>" . highlight-symbol)) ;; 按下 F3 键就可高亮当前符号
+
+;; C-c / t 触发 google-this，
+(use-package google-this
+  :ensure t
+  :init
+  (google-this-mode))
+
+(use-package tiny
+  :ensure t
+  ;; 可选绑定快捷键，笔者个人感觉不绑定快捷键也无妨
+  :bind
+  ("C-;" . tiny-expand))
 
 (provide 'init-config-packages)

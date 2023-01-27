4a54a05844abb4006fd0d9ad08f1c4f01de2ee72
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-01-27 22:50:56 +0800

    add package dirvish

--- elisp/init-config-packages.el
+++ elisp/init-config-packages.el
@@ -296,6 +296,7 @@ The cursor becomes a blinking bar, per `prot/cursor-type-mode'."
 
 (use-package iedit
   :ensure t)
+
 (use-package multiple-cursors
   :ensure t
   :after hydra
@@ -1266,6 +1267,59 @@ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cu
   (setq-default beacon-size 15)
   (add-hook 'after-init-hook 'beacon-mode)
   (beacon-mode 1))
+
+(use-package dirvish
+  :init
+  (dirvish-override-dired-mode)
+  :custom
+  (dirvish-quick-access-entries ; It's a custom option, `setq' won't work
+   '(("h" "~/"                          "Home")
+     ("d" "~/Downloads/"                "Downloads")
+     ("o" "~/Syncthings/org/"           "Org")
+     ("r" "~/Syncthings/org/roam/"      "Roam")))
+  :config
+  ;; (dirvish-peek-mode) ; Preview files in minibuffer
+  ;; (dirvish-side-follow-mode) ; similar to `treemacs-follow-mode'
+  (setq dirvish-mode-line-format
+        '(:left (sort symlink) :right (omit yank index)))
+  (setq dirvish-attributes
+        '(all-the-icons file-time file-size subtree-state vc-state git-msg))
+  (setq delete-by-moving-to-trash t)
+  (setq dired-listing-switches
+        "-l --almost-all --human-readable --group-directories-first --no-group")
+  :bind ; Bind `dirvish|dirvish-side|dirvish-dwim' as you see fit
+  (("C-c f" . dirvish-fd)
+   ("C-x d" . dirvish)
+   :map dirvish-mode-map ; Dirvish inherits `dired-mode-map'
+   ("a"   . dirvish-quick-access)
+   ("f"   . dirvish-file-info-menu)
+   ("y"   . dirvish-yank-menu)
+   ("N"   . dirvish-narrow)
+   ("^"   . dirvish-history-last)
+   ("-"   . dired-jump)
+   ("h"   . dirvish-history-jump) ; remapped `describe-mode'
+   ("s"   . dirvish-quicksort)    ; remapped `dired-sort-toggle-or-edit'
+   ("v"   . dirvish-vc-menu)      ; remapped `dired-view-file'
+   ("TAB" . dirvish-subtree-toggle)
+   ("M-f" . dirvish-history-go-forward)
+   ("M-b" . dirvish-history-go-backward)
+   ("M-l" . dirvish-ls-switches-menu)
+   ("M-m" . dirvish-mark-menu)
+   ("M-t" . dirvish-layout-toggle)
+   ("M-s" . dirvish-setup-menu)
+   ("M-e" . dirvish-emerge-menu)
+   ("M-j" . dirvish-fd-jump)))
+
+(dirvish-define-preview exa (file)
+                        "Use `exa' to generate directory preview."
+                        :require ("exa") ; tell Dirvish to check if we have the executable
+                        (when (file-directory-p file) ; we only interest in directories here
+                          `(shell . ("exa" "-al" "--color=always" "--icons"
+                                     "--group-directories-first" ,file))))
+
+(add-to-list 'dirvish-preview-dispatchers 'exa)
+
+(setq insert-directory-program "gls")
 
 (use-package s)
 (use-package dash

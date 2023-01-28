de0874de609932ba8fa8ff14b82e39873db7b99e
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-01-29 03:27:52 +0800

    set doom-dracula as default

--- elisp/init-config-packages.el
+++ elisp/init-config-packages.el
@@ -2731,12 +2731,14 @@ Up^^             Down^^           Miscellaneous           % 2(mc/num-cursors) cu
   ;; Global settings (defaults)
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-one t)
+  ;; (load-theme 'doom-one t)
+  ;; (load-theme 'doom-nord t)
+  (load-theme 'doom-dracula t)
 
   ;; Enable flashing mode-line on errors
   (doom-themes-visual-bell-config)
   ;; Enable custom neotree theme (all-the-icons must be installed!)
-  (doom-themes-neotree-config)
+  ;; (doom-themes-neotree-config)
   ;; or for treemacs users
   ;; (setq doom-themes-treemacs-theme "doom-atom") ; use "doom-colors" for less minimal icon theme
   ;; (doom-themes-treemacs-config)

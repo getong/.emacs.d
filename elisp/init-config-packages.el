2f1423841d7c8d36657cd3ed66b249edffeced67
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-01-30 20:30:16 +0800

    add php mode

--- elisp/init-config-packages.el
+++ elisp/init-config-packages.el
@@ -3895,5 +3895,35 @@ Activate this advice with:
 (use-package goto-last-change
   :bind (("C-x ;" . goto-last-change)))
 
+;; copy from https://qiita.com/tadsan/items/a76768439869f00a4e89
+(use-package php-mode
+  :hook ((php-mode . my-php-mode-setup))
+  :init
+  (require 'php-mode)
+  :custom
+  ;; (php-manual-url 'ja)
+  (php-mode-coding-style 'psr2)
+  (php-mode-template-compatibility nil)
+  :config
+  (defun my-php-mode-setup ()
+    "My PHP-mode hook."
+    (subword-mode 1)
+    (setq show-trailing-whitespace t)
+
+    (setq-local page-delimiter "\\_<\\(class\\|function\\|namespace\\)\\_>.+$")
+
+    (require 'flycheck-phpstan)
+    (flycheck-mode t)
+    (add-to-list 'flycheck-disabled-checkers 'php-phpmd)
+    (add-to-list 'flycheck-disabled-checkers 'php-phpcs))
+  ;; (bind-key "[" (smartchr "[]" "array()" "[[]]") php-mode-map)
+  ;; (bind-key "]" (smartchr "array " "]" "]]")     php-mode-map)
+  ;; (bind-key "C-}" 'cedit-barf php-mode-map)
+  ;; (bind-key "C-)" 'cedit-slurp php-mode-map)
+  (bind-key "C-c C-c" 'psysh-eval-region         php-mode-map)
+  (bind-key "<f6>" 'phpunit-current-project      php-mode-map)
+  (bind-key "C-c C--" 'php-current-class php-mode-map)
+  (bind-key "C-c C-=" 'php-current-namespace php-mode-map))
+
 (provide 'init-config-packages)
 ;;;; init-config-packages ends here

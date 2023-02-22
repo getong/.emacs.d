d4f176926bc61d18bee4ad47fa703d8295c2d8eb
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-02-23 00:37:59 +0800

    add nov package

--- elisp/init-config-packages.el
+++ elisp/init-config-packages.el
@@ -44,20 +44,134 @@
   (async-byte-compile-log-file (no-littering-expand-var-file-name "async-bytecomp.log")))
 
 ;; ensure straight install all the need packages
-(use-package f
-  :ensure t)
+(use-package f :ensure t)
 
 
 ;; hydra 更接近于「功能菜单」：弹出一个「常用功能列表」.
 ;; 你可以用连续击键来连续触发若干个函数。
-(use-package hydra
-  :ensure t
-  :commands defhydra
-  )
-
 (use-package use-package-hydra
   :ensure t
   :after hydra)
+
+(use-package pretty-hydra
+  :bind ("<f6>" . toggles-hydra/body)
+  :hook (emacs-lisp-mode . (lambda ()
+                             (add-to-list
+                              'imenu-generic-expression
+                              '("Hydras"
+                                "^.*(\\(pretty-hydra-define\\) \\([a-zA-Z-]+\\)"
+                                2))))
+  :init
+  (cl-defun pretty-hydra-title (title &optional icon-type icon-name
+                                      &key face height v-adjust)
+    "Add an icon in the hydra title."
+    (let ((face (or face `(:foreground ,(face-background 'highlight))))
+          (height (or height 1.0))
+          (v-adjust (or v-adjust 0.0)))
+      (concat
+       (when (and (icon-displayable-p) icon-type icon-name)
+         (let ((f (intern (format "all-the-icons-%s" icon-type))))
+           (when (fboundp f)
+             (concat
+              (apply f (list icon-name :face face :height height :v-adjust v-adjust))
+              " "))))
+       (propertize title 'face face))))
+
+  ;; Global toggles
+  (with-no-warnings
+    (pretty-hydra-define toggles-hydra (:title (pretty-hydra-title "Toggles" 'faicon "toggle-on" :v-adjust -0.1)
+                                               :color amaranth :quit-key ("q" "C-g"))
+      ("Basic"
+       (("n" (cond ((fboundp 'display-line-numbers-mode)
+                    (display-line-numbers-mode (if display-line-numbers-mode -1 1)))
+                   ((fboundp 'gblobal-linum-mode)
+                    (global-linum-mode (if global-linum-mode -1 1))))
+         "line number"
+         :toggle (or (bound-and-true-p display-line-numbers-mode)
+                     (bound-and-true-p global-linum-mode)))
+        ("a" global-aggressive-indent-mode "aggressive indent" :toggle t)
+        ("d" global-hungry-delete-mode "hungry delete" :toggle t)
+        ("e" electric-pair-mode "electric pair" :toggle t)
+        ("c" flyspell-mode "spell check" :toggle t)
+        ("s" prettify-symbols-mode "pretty symbol" :toggle t)
+        ("l" global-page-break-lines-mode "page break lines" :toggle t)
+        ("b" display-battery-mode "battery" :toggle t)
+        ("i" display-time-mode "time" :toggle t)
+        ("m" doom-modeline-mode "modern mode-line" :toggle t))
+       "Highlight"
+       (("h l" global-hl-line-mode "line" :toggle t)
+        ("h p" show-paren-mode "paren" :toggle t)
+        ("h s" symbol-overlay-mode "symbol" :toggle t)
+        ("h r" rainbow-mode "rainbow" :toggle t)
+        ("h w" (setq-default show-trailing-whitespace (not show-trailing-whitespace))
+         "whitespace" :toggle show-trailing-whitespace)
+        ("h d" rainbow-delimiters-mode "delimiter" :toggle t)
+        ("h i" highlight-indent-guides-mode "indent" :toggle t)
+        ("h t" global-hl-todo-mode "todo" :toggle t))
+       "Program"
+       (("f" flycheck-mode "flycheck" :toggle t)
+        ("F" flymake-mode "flymake" :toggle t)
+        ("O" hs-minor-mode "hideshow" :toggle t)
+        ("u" subword-mode "subword" :toggle t)
+        ("W" which-function-mode "which function" :toggle t)
+        ("E" toggle-debug-on-error "debug on error" :toggle (default-value 'debug-on-error))
+        ("Q" toggle-debug-on-quit "debug on quit" :toggle (default-value 'debug-on-quit))
+        ("v" global-diff-hl-mode "gutter" :toggle t)
+        ("V" diff-hl-flydiff-mode "live gutter" :toggle t)
+        ("M" diff-hl-margin-mode "margin gutter" :toggle t)
+        ("D" diff-hl-dired-mode "dired gutter" :toggle t))
+       "Theme"
+       (("t a" (centaur-load-theme 'auto) "auto"
+         :toggle (eq centaur-theme 'auto) :exit t)
+        ("t m" (centaur-load-theme 'random) "random"
+         :toggle (eq centaur-theme 'random) :exit t)
+        ("t s" (centaur-load-theme 'system) "system"
+         :toggle (eq centaur-theme 'system) :exit t)
+        ("t d" (centaur-load-theme 'default) "default"
+         :toggle (centaur-theme-enable-p 'default) :exit t)
+        ("t p" (centaur-load-theme 'pro) "pro"
+         :toggle (centaur-theme-enable-p 'pro) :exit t)
+        ("t k" (centaur-load-theme 'dark) "dark"
+         :toggle (centaur-theme-enable-p 'dark) :exit t)
+        ("t l" (centaur-load-theme 'light) "light"
+         :toggle (centaur-theme-enable-p 'light) :exit t)
+        ("t w" (centaur-load-theme 'warm) "warm"
+         :toggle (centaur-theme-enable-p 'warm) :exit t)
+        ("t c" (centaur-load-theme 'cold) "cold"
+         :toggle (centaur-theme-enable-p 'cold) :exit t)
+        ("t y" (centaur-load-theme 'day) "day"
+         :toggle (centaur-theme-enable-p 'day) :exit t)
+        ("t n" (centaur-load-theme 'night) "night"
+         :toggle (centaur-theme-enable-p 'night) :exit t)
+        ("t o" (ivy-read "Load custom theme: "
+                         (all-completions "doom" (custom-available-themes))
+                         :action (lambda (theme)
+                                   (centaur-set-variable
+                                    'centaur-theme
+                                    (let ((x (intern theme)))
+                                      (or (car (rassoc x centaur-theme-alist)) x)))
+                                   (counsel-load-theme-action theme))
+                         :caller 'counsel-load-theme)
+         "others"
+         :toggle (not (or (rassoc (car custom-enabled-themes) centaur-theme-alist)
+                          (rassoc (cadr custom-enabled-themes) centaur-theme-alist)))
+         :exit t))
+       "Package Archive"
+       (("p m" (centaur-set-package-archives 'melpa t)
+         "melpa" :toggle (eq centaur-package-archives 'melpa) :exit t)
+        ("p c" (centaur-set-package-archives 'emacs-cn t)
+         "emacs-cn" :toggle (eq centaur-package-archives 'emacs-cn) :exit t)
+        ("p b" (centaur-set-package-archives 'bfsu t)
+         "bfsu" :toggle (eq centaur-package-archives 'bfsu) :exit t)
+        ("p n" (centaur-set-package-archives 'netease t)
+         "netease" :toggle (eq centaur-package-archives 'netease) :exit t)
+        ("p s" (centaur-set-package-archives 'sjtu t)
+         "sjtu" :toggle (eq centaur-package-archives 'sjtu) :exit t)
+        ("p t" (centaur-set-package-archives 'tuna t)
+         "tuna" :toggle (eq centaur-package-archives 'tuna) :exit t)
+        ("p u" (centaur-set-package-archives 'ustc t)
+         "ustc" :toggle (eq centaur-package-archives 'ustc) :exit t)
+        ("p T" (centaur-test-package-archives) "speed test" :exit t))))))
 
 ;; benchmark-init records startup time by package so we can debug. It only records things after it's initialised, so put as early in config as possible.
 (use-package benchmark-init
@@ -3354,12 +3468,30 @@ Similar to `marginalia-annotate-symbol', but does not show symbol class."
 (use-package dumb-jump
   :ensure t
   :diminish dumb-jump-mode
-  :config
+  :pretty-hydra
+  ((:title (pretty-hydra-title "Dump Jump" 'faicon "anchor")
+    :color blue :quit-key ("q" "C-g"))
+   ("Jump"
+    (("j" dumb-jump-go "Go")
+     ("o" dumb-jump-go-other-window "Go other window")
+     ("e" dumb-jump-go-prefer-external "Go external")
+     ("x" dumb-jump-go-prefer-external-other-window "Go external other window"))
+    "Other"
+    (("i" dumb-jump-go-prompt "Prompt")
+     ("l" dumb-jump-quick-look "Quick look")
+     ("b" dumb-jump-back "Back"))))
+  :bind (("M-g o" . dumb-jump-go-other-window)
+         ("M-g j" . dumb-jump-go)
+         ("M-g i" . dumb-jump-go-prompt)
+         ("M-g x" . dumb-jump-go-prefer-external)
+         ("M-g z" . dumb-jump-go-prefer-external-other-window)
+         ("C-M-j" . dumb-jump-hydra/body))
+  :init
   (add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
+  (setq dumb-jump-prefer-searcher 'rg)
+  :config
   (setq xref-show-definitions-function #'xref-show-definitions-completing-read)
-  :bind (("C-M-g" . dumb-jump-go)
-         ("C-M-p" . dumb-jump-back)
-         ("C-M-q" . dumb-jump-quick-look)))
+ )
 
 
 ;; Color Identifier
@@ -4287,7 +4419,17 @@ deletion, or > if it is flagged for displaying."
   :custom
   (counsel-describe-function-function #'helpful-callable)
   (counsel-describe-variable-function #'helpful-variable)
+   :after pretty-hydra
+  :pretty-hydra
+  ((:color teal :quit-key "q")
+   ("Helpful"
+    (("f" helpful-callable "callable")
+     ("v" helpful-variable "variable")
+     ("k" helpful-key "key")
+     ("c" helpful-command "command")
+     ("d" helpful-at-point "thing at point"))))
   :bind
+  ("M-C-h" . helpful-hydra/body)
   ([remap describe-function] . helpful-function)
   ([remap describe-symbol] . helpful-symbol)
   ([remap describe-variable] . helpful-variable)
@@ -4659,126 +4801,6 @@ FACE defaults to inheriting from default and highlight."
   :config
   (reverse-im-mode t)) ; turn the mode on
 
-(use-package pretty-hydra
-  :bind ("<f6>" . toggles-hydra/body)
-  :hook (emacs-lisp-mode . (lambda ()
-                             (add-to-list
-                              'imenu-generic-expression
-                              '("Hydras"
-                                "^.*(\\(pretty-hydra-define\\) \\([a-zA-Z-]+\\)"
-                                2))))
-  :init
-  (cl-defun pretty-hydra-title (title &optional icon-type icon-name
-                                      &key face height v-adjust)
-    "Add an icon in the hydra title."
-    (let ((face (or face `(:foreground ,(face-background 'highlight))))
-          (height (or height 1.0))
-          (v-adjust (or v-adjust 0.0)))
-      (concat
-       (when (and (icon-displayable-p) icon-type icon-name)
-         (let ((f (intern (format "all-the-icons-%s" icon-type))))
-           (when (fboundp f)
-             (concat
-              (apply f (list icon-name :face face :height height :v-adjust v-adjust))
-              " "))))
-       (propertize title 'face face))))
-
-  ;; Global toggles
-  (with-no-warnings
-    (pretty-hydra-define toggles-hydra (:title (pretty-hydra-title "Toggles" 'faicon "toggle-on" :v-adjust -0.1)
-                                               :color amaranth :quit-key ("q" "C-g"))
-      ("Basic"
-       (("n" (cond ((fboundp 'display-line-numbers-mode)
-                    (display-line-numbers-mode (if display-line-numbers-mode -1 1)))
-                   ((fboundp 'gblobal-linum-mode)
-                    (global-linum-mode (if global-linum-mode -1 1))))
-         "line number"
-         :toggle (or (bound-and-true-p display-line-numbers-mode)
-                     (bound-and-true-p global-linum-mode)))
-        ("a" global-aggressive-indent-mode "aggressive indent" :toggle t)
-        ("d" global-hungry-delete-mode "hungry delete" :toggle t)
-        ("e" electric-pair-mode "electric pair" :toggle t)
-        ("c" flyspell-mode "spell check" :toggle t)
-        ("s" prettify-symbols-mode "pretty symbol" :toggle t)
-        ("l" global-page-break-lines-mode "page break lines" :toggle t)
-        ("b" display-battery-mode "battery" :toggle t)
-        ("i" display-time-mode "time" :toggle t)
-        ("m" doom-modeline-mode "modern mode-line" :toggle t))
-       "Highlight"
-       (("h l" global-hl-line-mode "line" :toggle t)
-        ("h p" show-paren-mode "paren" :toggle t)
-        ("h s" symbol-overlay-mode "symbol" :toggle t)
-        ("h r" rainbow-mode "rainbow" :toggle t)
-        ("h w" (setq-default show-trailing-whitespace (not show-trailing-whitespace))
-         "whitespace" :toggle show-trailing-whitespace)
-        ("h d" rainbow-delimiters-mode "delimiter" :toggle t)
-        ("h i" highlight-indent-guides-mode "indent" :toggle t)
-        ("h t" global-hl-todo-mode "todo" :toggle t))
-       "Program"
-       (("f" flycheck-mode "flycheck" :toggle t)
-        ("F" flymake-mode "flymake" :toggle t)
-        ("O" hs-minor-mode "hideshow" :toggle t)
-        ("u" subword-mode "subword" :toggle t)
-        ("W" which-function-mode "which function" :toggle t)
-        ("E" toggle-debug-on-error "debug on error" :toggle (default-value 'debug-on-error))
-        ("Q" toggle-debug-on-quit "debug on quit" :toggle (default-value 'debug-on-quit))
-        ("v" global-diff-hl-mode "gutter" :toggle t)
-        ("V" diff-hl-flydiff-mode "live gutter" :toggle t)
-        ("M" diff-hl-margin-mode "margin gutter" :toggle t)
-        ("D" diff-hl-dired-mode "dired gutter" :toggle t))
-       "Theme"
-       (("t a" (centaur-load-theme 'auto) "auto"
-         :toggle (eq centaur-theme 'auto) :exit t)
-        ("t m" (centaur-load-theme 'random) "random"
-         :toggle (eq centaur-theme 'random) :exit t)
-        ("t s" (centaur-load-theme 'system) "system"
-         :toggle (eq centaur-theme 'system) :exit t)
-        ("t d" (centaur-load-theme 'default) "default"
-         :toggle (centaur-theme-enable-p 'default) :exit t)
-        ("t p" (centaur-load-theme 'pro) "pro"
-         :toggle (centaur-theme-enable-p 'pro) :exit t)
-        ("t k" (centaur-load-theme 'dark) "dark"
-         :toggle (centaur-theme-enable-p 'dark) :exit t)
-        ("t l" (centaur-load-theme 'light) "light"
-         :toggle (centaur-theme-enable-p 'light) :exit t)
-        ("t w" (centaur-load-theme 'warm) "warm"
-         :toggle (centaur-theme-enable-p 'warm) :exit t)
-        ("t c" (centaur-load-theme 'cold) "cold"
-         :toggle (centaur-theme-enable-p 'cold) :exit t)
-        ("t y" (centaur-load-theme 'day) "day"
-         :toggle (centaur-theme-enable-p 'day) :exit t)
-        ("t n" (centaur-load-theme 'night) "night"
-         :toggle (centaur-theme-enable-p 'night) :exit t)
-        ("t o" (ivy-read "Load custom theme: "
-                         (all-completions "doom" (custom-available-themes))
-                         :action (lambda (theme)
-                                   (centaur-set-variable
-                                    'centaur-theme
-                                    (let ((x (intern theme)))
-                                      (or (car (rassoc x centaur-theme-alist)) x)))
-                                   (counsel-load-theme-action theme))
-                         :caller 'counsel-load-theme)
-         "others"
-         :toggle (not (or (rassoc (car custom-enabled-themes) centaur-theme-alist)
-                          (rassoc (cadr custom-enabled-themes) centaur-theme-alist)))
-         :exit t))
-       "Package Archive"
-       (("p m" (centaur-set-package-archives 'melpa t)
-         "melpa" :toggle (eq centaur-package-archives 'melpa) :exit t)
-        ("p c" (centaur-set-package-archives 'emacs-cn t)
-         "emacs-cn" :toggle (eq centaur-package-archives 'emacs-cn) :exit t)
-        ("p b" (centaur-set-package-archives 'bfsu t)
-         "bfsu" :toggle (eq centaur-package-archives 'bfsu) :exit t)
-        ("p n" (centaur-set-package-archives 'netease t)
-         "netease" :toggle (eq centaur-package-archives 'netease) :exit t)
-        ("p s" (centaur-set-package-archives 'sjtu t)
-         "sjtu" :toggle (eq centaur-package-archives 'sjtu) :exit t)
-        ("p t" (centaur-set-package-archives 'tuna t)
-         "tuna" :toggle (eq centaur-package-archives 'tuna) :exit t)
-        ("p u" (centaur-set-package-archives 'ustc t)
-         "ustc" :toggle (eq centaur-package-archives 'ustc) :exit t)
-        ("p T" (centaur-test-package-archives) "speed test" :exit t))))))
-
 ;; Rectangle
 (use-package rect
   :ensure nil
@@ -5105,6 +5127,32 @@ FACE defaults to inheriting from default and highlight."
   :straight
   (query-replace-many :type git :host github :repo "slotThe/query-replace-many")
   :bind ("C-M-%" . query-replace-many))
+
+
+;; Epub reader
+(use-package nov
+  :ensure t
+  :mode ("\\.epub\\'" . nov-mode)
+  :hook (nov-mode . my-nov-setup)
+  :init
+  (defun my-nov-setup ()
+    "Setup `nov-mode' for better reading experience."
+    (visual-line-mode 1)
+    (centaur-read-mode)
+    (face-remap-add-relative 'variable-pitch :family "Times New Roman" :height 1.5))
+  :config
+  (with-no-warnings
+    ;; WORKAROUND: errors while opening `nov' files with Unicode characters
+    ;; @see https://github.com/wasamasa/nov.el/issues/63
+    (defun my-nov-content-unique-identifier (content)
+      "Return the the unique identifier for CONTENT."
+      (let* ((name (nov-content-unique-identifier-name content))
+             (selector (format "package>metadata>identifier[id='%s']"
+                               (regexp-quote name)))
+             (id (car (esxml-node-children (esxml-query selector content)))))
+        (and id (intern id))))
+    (advice-add #'nov-content-unique-identifier :override #'my-nov-content-unique-identifier))
+  )
 
 (provide 'init-config-packages)
 ;;;; init-config-packages ends here

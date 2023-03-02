bb48187a9a8ff1e3c3abdef706b3864d85eba63c
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-03-02 11:02:16 +0800

    format

--- elisp/init-config-packages.el
+++ elisp/init-config-packages.el
@@ -1693,7 +1693,7 @@ Get it from:  <http://hasseg.org/trash/>"
   (add-to-list 'dirvish-preview-dispatchers 'exa)
 
   (when (eq system-type 'darwin)
-  (setq insert-directory-program "gls"))
+    (setq insert-directory-program "gls"))
   ;; 不预览epub文件
   (setq dirvish-preview-dispatchers (remove 'epub dirvish-preview-dispatchers))
   ;; 异步读取含 10000 个以上文件的文件夹

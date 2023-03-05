aa3878c822d04fe99bd1b481a4a756de0dfd1bf6
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-03-06 06:30:08 +0800

    update magit config, add forge

--- elisp/init-config-packages.el
+++ elisp/init-config-packages.el
@@ -3097,6 +3097,10 @@ Similar to `marginalia-annotate-symbol', but does not show symbol class."
       :foreground "red"
       :weight bold))))
   (magit-section-highlight ((t (:background "gray20"))))
+  :config
+  (setq auth-sources '("~/.authinfo"))
+  (add-hook 'magit-process-find-password-functions
+            'magit-process-password-auth-source)
   :init
   (setq magit-process-extreme-logging t)
   (setq magit-git-debug t)
@@ -5514,6 +5518,25 @@ Install the doc if it's not installed."
   (setq minimap-major-modes '(prog-mode))
   ;;(setq minimap-update-delay 0)
   (light-minimap))
+
+;; Access Git forges from Magit
+(use-package forge
+  :ensure magit
+  :demand t
+  :defines (forge-database-connector forge-topic-list-columns)
+  :custom-face
+  (forge-topic-label ((t (:inherit variable-pitch :height 0.9 :width condensed :weight regular :underline nil))))
+  :init
+  (setq forge-database-connector (if (and (require 'emacsql-sqlite-builtin nil t)
+                                          (functionp 'emacsql-sqlite-builtin)
+                                          (functionp 'sqlite-open))
+                                     'sqlite-builtin
+                                   'sqlite)
+        forge-topic-list-columns
+        '(("#" 5 forge-topic-list-sort-by-number (:right-align t) number nil)
+          ("Title" 60 t nil title  nil)
+          ("State" 6 t nil state nil)
+          ("Updated" 10 t nil updated nil))))
 
 (provide 'init-config-packages)
 ;;; init-config-packages ends here

eac2e2e0c1863a115fe175ae5596e7c50b2a7e80
19年梦醒 <3949379+getong@users.noreply.github.com>
2023-03-05 14:44:57 +0800

    use ghproxy

--- early-init.el
+++ early-init.el
@@ -89,15 +89,22 @@
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" straight-base-dir))
-      (bootstrap-version 6))
+      (bootstrap-version 5))
   (unless (file-exists-p bootstrap-file)
     (with-current-buffer
         (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/radian-software/straight.el/develop/install.el"
+         "https://ghproxy.com/https://raw.githubusercontent.com/sangjeedondrub/commons/main/install.el"
          'silent 'inhibit-cookies)
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
+
+;; Faster `straight' by using Github mirror in mainland China
+(defun sangjee/set-github-mirror (oldfunc &rest args)
+  (let ((url (apply oldfunc args)))
+    (replace-regexp-in-string (rx (group "https://github.com"))
+                              "https://ghproxy.com/https://github.com" url nil nil 1)))
+(advice-add 'straight-vc-git--encode-url :around #'sangjee/set-github-mirror)
 
 (setq byte-compile-warnings '(not cl-functions obsolete))
 
